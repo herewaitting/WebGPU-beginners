e3762c7fb98d869e776f8bc438c85114dfdee3e6
shitingchun <651978103@qq.com>
2023-02-03 20:32:38 +0800

    init vite evn

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import GpuTest from './components/gputest/GpuTest.vue'
+</script>
+
+<template>
+  <GpuTest/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/gputest/GpuTest.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <canvas id = "App"></canvas>
+</template>
+
+<style scoped>
+#App {
+  width: 100%;
+  height: 100%;
+}
+</style>
